3973a76846831dc34cbdcaf36daa7a10b322fca1
Your name <username@mailserver.com>
2024-04-26 12:13:17 -0700

    initial checkin

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,13 @@
 package trees;
 
-public class TreeException extends FamilyTree
+import java.io.IOException;
+
+public class TreeException extends Exception
 {
 	public TreeException(String s)
 	{
 		// Pass s into a superclass ctor.
+		super(s);
+		
 	}
 }
